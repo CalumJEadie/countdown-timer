10b5666fb54605e00365f455ccd39a95d0b9ef4e
Calum J. Eadie <calum@calumjeadie.com>
2011-12-24 01:06:20 +0000

    Have added label, spinner and button to selector.

--- Countdown Timer/src/com/calumjeadie/dev/countdown_timer/TimeSelector.java
+++ Countdown Timer/src/com/calumjeadie/dev/countdown_timer/TimeSelector.java
@@ -1,11 +1,14 @@
 package com.calumjeadie.dev.countdown_timer;
 
-import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.util.Calendar;
 import java.util.Date;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -33,18 +36,30 @@ public class TimeSelector extends JFrame {
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        panel = new JPanel(new BorderLayout(20,20));
+        panel = new JPanel(new GridLayout(0,1,10,10)); // 1 column, as many rows as necessary
+        //GridLayout g = new GridLayout();
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         
         JLabel label = new JLabel("Select a time");
-        label.setFont(new Font("Georgia", Font.PLAIN, 14));
+        label.setFont(new Font("Dialog", Font.PLAIN, 16));
         
-        panel.add(label, BorderLayout.CENTER);
+        panel.add(label);
         
-        targetDate = new Date();
-        targetDateModel = new SpinnerDateModel(targetDate,new Date(),null,Calendar.MILLISECOND);
+        Date now = new Date();
+        targetDate = now;
+        targetDateModel = new SpinnerDateModel(targetDate,now,null,Calendar.MILLISECOND);
 
         JSpinner spinner = new JSpinner(targetDateModel);
+       
+        JSpinner.DateEditor spinnerEditor = new JSpinner.DateEditor(spinner,"dd/MM/yyyy H:m:s");
+        
+        //spinnerEditor.setFont(new Font("Georgia", Font.PLAIN, 20));
+        
+        panel.add(spinner);
+        
+        JButton countdownButton = new JButton("Start Countdown");
+        
+        panel.add(countdownButton);
         
         add(panel);
         
