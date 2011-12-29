e664b8ea1258348ff7c2d7d00398c4165b3f13bb
Calum J. Eadie <calum@calumjeadie.com>
2011-12-29 16:01:19 +0000

    Added MIT licensing.

--- Countdown Timer/src/com/calumjeadie/dev/countdown_timer/CountdownTimer.java
+++ Countdown Timer/src/com/calumjeadie/dev/countdown_timer/CountdownTimer.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Calum J. Eadie.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License
+ * which accompanies this distribution, and is available at
+ * http://www.opensource.org/licenses/MIT
+ * 
+ * Contributors:
+ *     Calum J. Eadie - initial API and implementation
+ ******************************************************************************/
 package com.calumjeadie.dev.countdown_timer;
 
 import java.awt.Font;
